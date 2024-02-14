fbd380980b3c9b0f2a12c6a8b12e77ffc9fb9277
Martin Brož <martin.broz@online.txn.cz>
2024-02-14 12:58:43 +0100

    person API get,get, post

---
+++ InsuranceCorp.WEBAPI/Controllers/PersonController.cs
@@ -0,0 +1,53 @@
+﻿using InsuranceCorp.Data;
+using InsuranceCorp.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace InsuranceCorp.WEBAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly InsCorpDbContext db;
+
+        public PersonController(InsCorpDbContext db)
+        {
+            this.db = db;
+        }
+
+        //[Route("List")]
+        [HttpGet("List")]
+        public ActionResult<List<Person>> List(int start, int take)
+        {
+            return db.Persons.Skip(start).Take(take).ToList();
+        }
+
+        [HttpGet("{id:int}")]
+        public ActionResult<Person> getPerson(int id)
+        {
+            var person = db.Persons
+                .Include(x => x.Address)
+                .Include(x => x.Contracts)
+                .FirstOrDefault(x => x.Id == id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return person;
+        }
+
+        [HttpPost("create")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public ActionResult postPerson(Person person)
+        {
+            db.Persons.Add(person);
+            db.SaveChanges();
+
+            return Created($"/api/person/{person.Id}", person);
+        }
+    }
+}
